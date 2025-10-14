6057aa826bb8e8d2ef45cba7c2c88e319ec116ef
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-10-14 11:37:06 -0300

    feat(slot): send google invite when reserving

--- run/main.go
+++ run/main.go
@@ -4,35 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 	"os"
 	"time"
+
+	"memudo/run/smtp"
 )
 
 func main() {
-	target, _ := url.Parse("http://app:5173")
-
-	proxy := httputil.NewSingleHostReverseProxy(target)
-
-	// Optionally log/transform in one place (future telemetry, error wrapping, etc.)
-	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
-		log.Printf("proxy error: %v", err)
-		http.Error(w, "upstream error", http.StatusBadGateway)
-	}
-
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok"))
 	})
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// set forward headers for SvelteKit
-		r.Header.Set("X-Forwarded-Proto", "http")
-		r.Header.Set("X-Forwarded-Host", r.Host)
-		r.Header.Set("X-Forwarded-For", r.RemoteAddr)
-		proxy.ServeHTTP(w, r)
-	})
+	http.HandleFunc("/send-email", smtp.Handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -40,10 +24,10 @@ func main() {
 	}
 
 	addr := fmt.Sprintf(":%s", port)
-	s := &http.Server{
+	server := &http.Server{
 		Addr:              addr,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
-	log.Printf("Go proxy listening on %s → app:5173", addr)
-	log.Fatal(s.ListenAndServe())
+	log.Printf("Go listening on %s → app:5173", addr)
+	log.Fatal(server.ListenAndServe())
 }
