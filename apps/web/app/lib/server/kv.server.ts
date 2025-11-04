36bce8becd8084e656e519b38ec69658a407d6a5
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-11-04 20:38:52 -0300

    fix: replace the redis client with a node compatible

--- apps/web/app/lib/server/kv.server.ts
+++ apps/web/app/lib/server/kv.server.ts
@@ -1,39 +1,23 @@
-import { RedisClient } from "@iuioiua/redis"
-import { connect } from "node:net"
+import Redis from "ioredis"
 
 if (!process.env.REDIS_URL) throw new Error("REDIS_URL is not set")
 
-const url = new URL(process.env.REDIS_URL)
-const port = Number.parseInt(url.port)
-const hostname = url.hostname
-
-if (!port) throw new Error("REDIS_URL must include a port")
-if (!hostname) throw new Error("REDIS_URL must include a hostname")
-
-const connection = connect({ port, host: hostname })
-const client = new RedisClient(connection)
+const client = new Redis(process.env.REDIS_URL)
 
 export const kv = {
   async del(key: string): Promise<number> {
-    return (await client.sendCommand(["DEL", key])) as number
+    return await client.del(key)
   },
 
   async hlen(key: string): Promise<number> {
-    return (await client.sendCommand(["HLEN", key])) as number
+    return await client.hlen(key)
   },
 
   async hgetall(key: string): Promise<Record<string, string> | null> {
-    const result = (await client.sendCommand(["HGETALL", key])) as string[]
-    if (result.length === 0) return null
-
-    const hash: Record<string, string> = {}
-    for (let i = 0; i < result.length; i += 2) {
-      hash[result[i]] = result[i + 1]
-    }
-    return hash
+    return await client.hgetall(key)
   },
 
   async hmset(key: string, fields: string[]): Promise<string> {
-    return (await client.sendCommand(["HMSET", key, ...fields])) as string
+    return await client.hmset(key, ...fields)
   },
 }
