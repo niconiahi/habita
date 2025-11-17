da2fec5d99f5d89f8d3652cfb7e246b939ee2ba4
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-11-17 18:33:17 -0300

    fix(production): login redirect works

--- apps/web/app/lib/server/google.server.ts
+++ apps/web/app/lib/server/google.server.ts
@@ -4,9 +4,11 @@ if (!process.env.GOOGLE_CLIENT_ID)
   throw new Error("GOOGLE_CLIENT_ID is not set")
 if (!process.env.GOOGLE_CLIENT_SECRET)
   throw new Error("GOOGLE_CLIENT_SECRET is not set")
+if (!process.env.GOOGLE_REDIRECT_URL)
+  throw new Error("GOOGLE_REDIRECT_URL is not set")
 
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "https://dev.habita.rent/login/google/callback",
+  process.env.GOOGLE_REDIRECT_URL,
 )
