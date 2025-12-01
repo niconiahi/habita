179520df9e0f7e204d1eb55be5f2c79f181cdcf9
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-12-01 00:32:40 -0300

    feat: add image carousel component

--- apps/web/app/routes/properties+/_index.tsx
+++ apps/web/app/routes/properties+/_index.tsx
@@ -1,12 +1,31 @@
 import { display_location } from "~/lib/display_location"
-import { get_img_props } from "~/lib/image"
+import { get_img_props } from "~/lib/image.server"
 import type { Route } from "./+types/_index"
 import { fetch_properties } from "./fetchers/server/properties.server"
 import { Link } from "react-router"
+import { Carousel } from "~/components/carousel"
 
 export async function loader() {
   const properties = await fetch_properties()
-  return { properties }
+
+  // Generate image props on the server
+  const properties_with_image_props = properties.map(
+    (property) => ({
+      ...property,
+      images: property.images.map((image, index) => ({
+        ...image,
+        props: get_img_props(image.id, image.hash, {
+          widths: [400, 800],
+          sizes: [
+            "(max-width: 600px) 400px, (max-width: 900px) 800px",
+          ],
+        }),
+        alt: `property at ${display_location(property.location)} - image ${index + 1}`,
+      })),
+    }),
+  )
+
+  return { properties: properties_with_image_props }
 }
 
 export default function ({
@@ -21,52 +40,19 @@ export default function ({
           const key = `property-${property.id}`
           return (
             <li key={key}>
-              <div
-                style={{
-                  display: "flex",
-                  overflowX: "auto",
-                  scrollSnapType: "x mandatory",
-                  WebkitOverflowScrolling: "touch",
-                  scrollbarWidth: "none",
-                  msOverflowStyle: "none",
-                }}
-              >
-                {property.images.map((image) => {
-                  const id = `img-${property.id}`
+              <Carousel.Root>
+                {property.images.map((image, index) => {
+                  const id = `img-${property.id}-${index}`
                   return (
-                    <Link
-                      key={id}
-                      to={`${property.id}`}
-                      style={{
-                        aspectRatio: "4 / 3",
-                        flex: "0 0 100%",
-                        scrollSnapAlign: "start",
-                        scrollSnapStop: "always",
-                      }}
-                    >
+                    <Carousel.Item key={id}>
                       <img
-                        {...get_img_props(
-                          image.id,
-                          image.hash,
-                          {
-                            widths: [400, 800],
-                            sizes: [
-                              "(max-width: 600px) 400px, (max-width: 900px) 800px",
-                            ],
-                          },
-                        )}
-                        alt={`Property at ${display_location(property.location)}`}
-                        style={{
-                          width: "100%",
-                          height: "100%",
-                          objectFit: "cover",
-                          display: "block",
-                        }}
+                        {...image.props}
+                        alt={image.alt}
                       />
-                    </Link>
+                    </Carousel.Item>
                   )
                 })}
-              </div>
+              </Carousel.Root>
               <Link to={`${property.id}`}>
                 propiedad localizada en{" "}
                 {display_location(property.location)}
