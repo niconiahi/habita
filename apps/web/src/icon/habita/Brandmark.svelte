70fea0c1b7f27f05d0e9b991615803c207dc8ff4
Nicolas Accetta <nicolas.accetta@gmail.com>
2026-04-16 19:12:57 -0300

    feat(design): add icon components for bell, chevron, google and habita brand

---
+++ apps/web/src/icon/habita/Brandmark.svelte
@@ -0,0 +1,7 @@
+<svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M19.666 3.77832C20.4523 3.17768 21.5429 3.17769 22.3291 3.77832L38.9395 16.4668C39.4827 16.8818 39.8017 17.5264 39.8018 18.21V37.6074C39.8018 38.819 38.819 39.8018 37.6074 39.8018H4.3877C3.1761 39.8018 2.19336 38.819 2.19336 37.6074V18.21C2.19345 17.5264 2.51248 16.8818 3.05566 16.4668L19.666 3.77832Z" fill="#00A5CF" stroke="#004E64" stroke-width="4.38755"/>
+<path d="M12.3403 38.9778L12.3403 11.6612" stroke="#004E64" stroke-width="4.38755" stroke-linecap="round"/>
+<path d="M12.3407 30.5761H29.6784" stroke="#004E64" stroke-width="4.38755" stroke-linecap="round"/>
+<path d="M29.6784 38.9772L29.6784 22.1187" stroke="#004E64" stroke-width="4.38755" stroke-linecap="round"/>
+<path d="M36.5901 13.9114L36.5901 9.41005" stroke="#004E64" stroke-width="4.38755" stroke-linecap="round"/>
+</svg>
