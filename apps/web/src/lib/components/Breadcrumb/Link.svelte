2205f35d44fbfe32034e5eb37dc95bfdec5e9994
Nicolas Accetta <nicolas.accetta@gmail.com>
2026-04-19 13:30:45 -0300

    feat: add breadcrumb compound component following w3c apg pattern

---
+++ apps/web/src/lib/components/Breadcrumb/Link.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { Snippet } from "svelte"
+
+  interface Props {
+    href: string
+    children: Snippet
+  }
+
+  let { href, children }: Props = $props()
+</script>
+
+<li>
+  <a {href} class="link">
+    {@render children()}
+  </a>
+</li>
+
+<style>
+  .link {
+    font: inherit;
+  }
+</style>
