3de8798d11589ec6edff98a98ffe5102335d8066
Nicolas Accetta <nicolas.accetta@gmail.com>
2026-04-24 09:59:04 -0300

    feat(candidates): add document list + show detailed personal info

--- apps/web/src/routes/admin/candidates/[candidate_id]/+page.server.ts
+++ apps/web/src/routes/admin/candidates/[candidate_id]/+page.server.ts
@@ -1,5 +1,5 @@
 import { require_authentication } from "$lib/server/auth"
-import { error, redirect } from "@sveltejs/kit"
+import { error } from "@sveltejs/kit"
 import { query_builder } from "db/query_builder"
 import * as v from "valibot"
 import { ACCESS_TYPE } from "$lib/access_type"
@@ -7,7 +7,10 @@ import { ForceNumberSchema } from "$lib/force_number"
 import { get_accessible_property_ids } from "$lib/server/property_access"
 import { SLOT_STATE } from "$lib/slot_state"
 import type { PageServerLoad } from "./$types"
-import { fetch_candidate } from "./fetchers/candidate.server"
+import {
+  fetch_candidate,
+  fetch_candidate_files,
+} from "./fetchers/candidate.server"
 
 export const load: PageServerLoad = async ({
   locals,
@@ -29,16 +32,22 @@ export const load: PageServerLoad = async ({
   const candidate_slot = await query_builder
     .selectFrom("slot")
     .where("slot.visitant_id", "=", candidate_id)
-    .where("slot.state", "=", SLOT_STATE.RESERVED)
+    .where("slot.state", "in", [
+      SLOT_STATE.RESERVED,
+      SLOT_STATE.CONFIRMED,
+    ])
     .where("slot.property_id", "in", property_ids)
     .select(["slot.id"])
     .executeTakeFirst()
   if (!candidate_slot) {
     error(403, "not authorized")
   }
-  const candidate = await fetch_candidate(candidate_id)
+  const [candidate, candidate_files] = await Promise.all([
+    fetch_candidate(candidate_id),
+    fetch_candidate_files(candidate_id),
+  ])
   if (!candidate) {
     error(404, "candidate not found")
   }
-  return { candidate }
+  return { candidate, candidate_files }
 }
