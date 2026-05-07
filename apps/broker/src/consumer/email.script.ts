89727388760bfd2c40d9f3c48ece8ef63bda57fa
Nicolas Accetta <nicolas.accetta@gmail.com>
2026-05-06 21:31:41 -0300

    feat(teams): add invitation email pipeline and signup auto-accept

--- apps/broker/src/consumer/email.script.ts
+++ apps/broker/src/consumer/email.script.ts
@@ -1,5 +1,6 @@
 import { SEND_BOOKING_CONFIRMATION_TOPIC } from "../event/send_booking_confirmation"
 import { SEND_LANDLORD_INVITE_TOPIC } from "../event/send_landlord_invite"
+import { SEND_TEAM_INVITE_TOPIC } from "../event/send_team_invite"
 import { SEND_RENEWAL_REMINDER_TOPIC } from "../event/send_renewal_reminder"
 import { SEND_SIGNING_REQUEST_TOPIC } from "../event/send_signing_request"
 import { SEND_NO_SLOTS_ALERT_TOPIC } from "../event/send_no_slots_alert"
@@ -8,6 +9,7 @@ import { SEND_SLOT_RESERVED_ALERT_TOPIC } from "../event/send_slot_reserved_aler
 import { create_consumer } from "./create_consumer"
 import { handle_send_booking_confirmation } from "../handler/handle_send_booking_confirmation"
 import { handle_send_landlord_invite } from "../handler/handle_send_landlord_invite"
+import { handle_send_team_invite } from "../handler/handle_send_team_invite"
 import { handle_send_no_slots_alert } from "../handler/handle_send_no_slots_alert"
 import { handle_send_renewal_reminder } from "../handler/handle_send_renewal_reminder"
 import { handle_send_signing_request } from "../handler/handle_send_signing_request"
@@ -24,6 +26,7 @@ create_consumer({
       handle_send_signing_request,
     [SEND_LANDLORD_INVITE_TOPIC]:
       handle_send_landlord_invite,
+    [SEND_TEAM_INVITE_TOPIC]: handle_send_team_invite,
     [SEND_RENEWAL_REMINDER_TOPIC]:
       handle_send_renewal_reminder,
     [SEND_SLOT_RESERVED_ALERT_TOPIC]:
